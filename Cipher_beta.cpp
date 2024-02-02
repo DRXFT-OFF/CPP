bb7d8880ea324cb68f5d7ac506db3ec49d193ab2
DRXFT-OFF <103736024+DRXFT-OFF@users.noreply.github.com>
2024-02-02 19:41:57 +0500

    Update v0.1

--- Cipher_beta.cpp
+++ Cipher_beta.cpp
@@ -1,54 +1,96 @@
 #include <iostream>
 using namespace std;
 
-const int key = 188;
-
-char* Cipher(char input[50]);
-char* Decipher(char* encrypted_input);
-void Print_Encrypted(char* encrypted_pointer);
-void Print_Decrypted(char* decrypted_pointer);
-
-int main()
+class Crypt
 {
-	cout << "Enter your string: ";
-	char ch[50];
-	cin.getline(ch, sizeof(ch));
+	const int key = 25;
+	char encrypt_char[50], decrypt_char[50];
 	char *encrypted, *decrypted;
+	public:
+		Crypt()
+		{
+			encrypted = encrypt_char;
+			decrypted = decrypt_char;
+		}
+		char* Encrypt(char* string);
+		char* Decrypt(char* string);
+		void Print_encrypt()
+		{
+			if (encrypt_char[0]) cout << encrypted;
+			else cout << "Error! String is empty!";
+		}
+		void Print_decrypt()
+		{
+			if (decrypt_char[0]) cout << decrypted;
+			else cout << "Error! String is empty!";
+		}
+};
 
-	encrypted = Cipher(ch);
-	decrypted = Decipher(encrypted);
-
-	Print_Encrypted(encrypted);
-	Print_Decrypted(decrypted);
-	return 0;
-}
-
-char* Cipher(char input[50])
+char* Crypt::Encrypt(char* string)
 {
-	char encrypted[50];
-	for (int i = 0; input[i] != '\0'; i++)
+	for (int i = 0; string[i]; i++)
 	{
-		encrypted[i] = input[i] ^ key;
+		encrypted[i] = string[i] ^ key;
 	}
 	return encrypted;
 }
 
-char* Decipher(char* encrypted_input)
+char* Crypt::Decrypt(char* string)
 {
-	char decrypted[50];
-	for (int i = 0; encrypted_input[i] != '\0'; i++)
+	for (int i = 0; string[i]; i++)
 	{
-		decrypted[i] = encrypted_input[i] ^ key;
+		decrypted[i] = string[i] ^ key;
 	}
 	return decrypted;
 }
 
-void Print_Encrypted(char* encrypted_pointer)
+int main()
 {
-	cout << "Encrypted: " << encrypted_pointer << "\n";
-}
-
-void Print_Decrypted(char* decrypted_pointer)
-{
-	cout << "Decrypted: " << decrypted_pointer << "\n";
+	Crypt crypt;
+	char input[50];
+	char choose;
+	while (true)
+	{
+		cout << "Choose: \n" << "1.Encrypt \n" << "2.Decrypt \n" << "3.Print last saved encrypt \n" << "4.Print last saved decrypt \n" << "5.Close programm \n";
+		cin >> choose;
+		cin.ignore();
+		switch (choose)
+		{
+		case '1':
+			cout << "Enter string to encrypt: ";
+			cin.getline(input, sizeof(input));
+			crypt.Encrypt(input);
+			cout << "Encrypted: ";
+			crypt.Print_encrypt();
+			cout << "\n";
+			break;
+		case '2':
+			cout << "Enter string to decrypt: ";
+			cin.getline(input, sizeof(input));
+			crypt.Decrypt(input);
+			cout << "Decrypted: ";
+			crypt.Print_decrypt();
+			cout << "\n";
+			break;
+		case '3':
+			cout << "Last saved encrypt: ";
+			crypt.Print_encrypt();
+			cout << "\n";
+			break;
+		case '4':
+			cout << "Last saved decrypt: ";
+			crypt.Print_decrypt();
+			cout << "\n";
+			break;
+		case '5':
+			exit(0);
+			break;
+		default:
+			cout << "Error! Wrong input!";
+			break;
+		}
+		cout << "\n";
+	}
+	
+	return 0;
 }
